061ac991820137ba99ede53058cfcc21739eb0d1
pdesai2 <Pratik.Desai@intuit.com>
2013-09-25 16:26:48 -0700

    Initial Commit

---
+++ GameTrader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameTrader
+//
+//  Created by Desai, Pratik on 9/25/13.
+//  Copyright (c) 2013 Desai, Pratik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
